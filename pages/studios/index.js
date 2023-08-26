cd0f44ec4f2ca41741675a6dd7f817e9f4eb583a
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-26 14:30:19 -0400

    created studio card to map through on the studios page, enroll/unenroll button working correctly

--- pages/studios/index.js
+++ pages/studios/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ChooseStudioForm from '../../components/studio/ChooseStudioForm';
+import React, { useState, useEffect } from 'react';
+import StudioCard from '../../components/studio/StudioCard';
+import { useAuth } from '../../utils/context/authContext';
+import { getStudios } from '../../utils/data/studioData';
 
-export default function chooseStudios() {
+function StudiosPage() {
+  const [studios, setStudios] = useState([]);
+  const { user } = useAuth();
+
+  const getAllStudios = () => {
+    getStudios(user.uid).then((data) => setStudios(data));
+  };
+
+  console.warn(studios);
+
+  useEffect(() => {
+    getAllStudios();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
   return (
-    <div>
-      <ChooseStudioForm />
-    </div>
+    <article className="studios">
+      <h1>Studios</h1>
+      {studios.map((studio) => (
+        <section key={`studio--${studio.id}`} className="studio">
+          <StudioCard studioObj={studio} onUpdate={getAllStudios} enrolled={studio.enrolled} />
+        </section>
+      ))}
+    </article>
   );
 }
+
+export default StudiosPage;
