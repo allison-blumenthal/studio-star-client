1b488abe8a32104ea8d0933ebf4e1177d0056522
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-26 15:39:01 -0400

    updated register form to route to new studio form for teachers, or studios page for students, created studio form, added dynamic studio details page, added new studio page

---
+++ pages/studios/[id].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function StudioHome() {
+  return (
+    <div>
+      <h1>
+        My Studio Page
+      </h1>
+    </div>
+  );
+}
---
+++ pages/studios/new.js
@@ -0,0 +1,10 @@
+import StudioForm from '../../components/studio/StudioForm';
+
+const NewStudio = () => (
+  <div>
+    <h2>Create Your Studio</h2>
+    <StudioForm />
+  </div>
+);
+
+export default NewStudio;
