8d9720da07dab31d6322fa67e0563b24a0798dd6
Allison Blumenthal <blumen.allison@gmail.com>
2023-08-28 20:01:25 -0400

    mapped through assignment card on student assignment page

--- components/assignment/AssignmentCard.js
+++ components/assignment/AssignmentCard.js
@@ -3,15 +3,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import Link from 'next/link';
+import Image from 'next/image';
 import assignment from '../../src/assets/images/assignment-icon.png';
 
 // eslint-disable-next-line no-unused-vars
 function AssignmentCard({ assignmentObj, onUpdate }) {
+  const date = new Date(assignmentObj.date).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+
   return (
     <Card className="text-center assignment-card" style={{ width: '18rem', margin: '10px' }}>
-      <Card.Body>Total: ${assignmentObj.date}</Card.Body>
+      <Card.Body>Date: {date}</Card.Body>
       <Link href={`/assignments/${assignmentObj.id}`} passHref>
-        <Card.Img className="assignment-icon" src={assignment} alt="assignment-icon" />
+        <div className="assignment-icon">
+          <Image src={assignment} alt="assignment icon" />
+        </div>
       </Link>
     </Card>
   );
--- pages/students/[id].js
+++ pages/students/[id].js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import getSingleUser from '../../utils/data/userData';
+import { getAssignmentsByStudentId } from '../../utils/data/assignmentData';
+import AssignmentCard from '../../components/assignment/AssignmentCard';
 
 export default function StudentAssignments() {
   const [student, setStudent] = useState({});
+  const [assignments, setAssignments] = useState([]);
   const router = useRouter();
 
   const { id } = router.query;
@@ -12,14 +15,26 @@ export default function StudentAssignments() {
     getSingleUser(id).then((data) => setStudent(data));
   };
 
+  const getStudentAssignments = () => {
+    getAssignmentsByStudentId(id).then((data) => setAssignments(data));
+  };
+
   useEffect(() => {
     getCurrentStudent();
+    getStudentAssignments();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   return (
     <div>
       <h1>{student.first_name}&apos;s Assignments</h1>
+      {assignments.map((assignment) => (
+        <section key={`assignment--${assignment.id}`} className="assignment">
+          <AssignmentCard assignmentObj={assignment} onUpdate={getStudentAssignments} />
+          <br />
+        </section>
+      ))}
+
     </div>
   );
 }
