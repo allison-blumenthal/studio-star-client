d7121a2fed51d0b46d285ae8206a8efae1e92152
Allison Blumenthal <blumen.allison@gmail.com>
2023-09-19 14:42:48 -0400

    styled studio student card

--- components/studio/StudioStudentCard.js
+++ components/studio/StudioStudentCard.js
@@ -20,25 +20,32 @@ function StudioStudentCard({ studioStudentObj, onUpdate }) {
     router.push(`/students/${studioStudentObj.student_id.id}`);
   };
 
+  // const handleProfile = () => {
+  //   router.push(`/profile/${studioStudentObj.student_id.id}`);
+  //   console.warn(studioStudentObj.student_id.id);
+  // };
+
   return (
     <div className="bg-white rounded-lg shadow-lg p-4 md:p-6 mb-6">
       <div className="flex">
-        <h3 className="text-lg font-semibold mb-2">
+        {/* <button onClick={handleProfile} type="button"> */}
+        <h2 className="text-lg font-semibold bevan mb-2">
           {studioStudentObj.student_id.first_name} {studioStudentObj.student_id.last_name}
-        </h3>
-        <p className="text-gray-600 ml-auto">{studioStudentObj.student_id.instrument}</p>
+        </h2>
+        {/* </button> */}
+        <h3 className="text-gray-600 ml-auto coustard">{studioStudentObj.student_id.instrument}</h3>
       </div>
-      <div className="flex justify-between items-center mb-4">
-        <button onClick={handleClick} type="button">
-          <Image src={assignment} alt="assignment icon" width={24} height={24} />
-        </button>
+      <h3 className="text-gray-600 mb-2 coustard italic">{studioStudentObj.student_id.pronouns}</h3>
+      <div className="text-center">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={studioStudentObj.student_id.profile_image_url} alt="student" className="rounded-2xl mt-4 inline-block" />
       </div>
-      <p className="text-gray-600 mb-2">{studioStudentObj.student_id.pronouns}</p>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={studioStudentObj.student_id.profile_image_url} alt="student" className="rounded-2xl mt-4" />
-      <div className="text-right">
-        <button onClick={unenroll} type="button">
+      <div className="pt-4 flex justify-between">
+        <button onClick={unenroll} type="button" className="bg-orange-400 hover:bg-orange-300 text-white px-3 pb-1 pt-2 rounded">
           <Image src={trash} alt="remove student icon" width={20} height={20} />
+        </button>
+        <button onClick={handleClick} type="button" className="bg-blue-400 hover:bg-blue-300 text-white px-3 pb-1 pt-2 rounded">
+          <Image src={assignment} alt="assignment icon" width={20} height={20} />
         </button>
       </div>
     </div>
