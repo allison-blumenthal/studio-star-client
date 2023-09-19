2733ffc3a13bc2d691b36109296d542d588e28b4
Allison Blumenthal <blumen.allison@gmail.com>
2023-09-18 23:33:05 -0400

    tweaked home page, working on profile page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
+import Head from 'next/head';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth();
 
   return (
-    <div className="min-h-screen flex flex-col justify-start items-center">
-      <div className="max-w-md p-4 md:p-6 bg-white rounded-lg shadow-lg mt-4">
-        <h1 className="text-3xl font-semibold mb-4">
-          Welcome to Studio Star, {user.first_name}!
-        </h1>
-        <h2 className="text-lg text-gray-600">
-          {user.is_teacher === true
-            ? 'Thank you for using our platform for all your music teaching studio needs.'
-            : 'Thank you for using our platform to access all your music lesson materials.'}
-        </h2>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="min-h-screen flex flex-col justify-start items-center">
+        <div className="max-w-lg p-4 md:p-6 bg-white rounded-lg shadow-lg mt-4 mx-4">
+          <h1 className="text-3xl font-semibold mb-4 text-center bevan">
+            Welcome, {user.first_name}!
+          </h1>
+          <h2 className="text-lg text-gray-600 coustard">
+            {user.is_teacher === true
+              ? 'Thank you for using our platform for all your studio needs. Use the navbar to access your roster and profile.'
+              : 'Thank you for using our platform to access all your music lesson materials.'}
+          </h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import { useAuth } from '../../utils/context/authContext';
 
 export default function ProfileDetails() {
@@ -12,20 +13,29 @@ export default function ProfileDetails() {
   };
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <h2>First name: {user.first_name}</h2>
-      <h2>Last name: {user.last_name}</h2>
-      <h2>Pronouns: {user.pronouns}</h2>
-      <h2>Instrument: {user.instrument}</h2>
-      <h2>Email address: {user.email}</h2>
-      {user.is_teacher === false ? (
-        <>
-          <h2>Birthdate: {user.birthdate}</h2>
-          <h2>Guardian names: {user.guardian_names}</h2>
-          <Button onClick={handleClick}>View Teachers</Button>
-        </>
-      ) : ('')}
-    </div>
+    <>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <div className="p-4 coustard">
+        <h1 className="bevan text-xl mb-2">Profile</h1>
+        <h2>First name: {user.first_name}</h2>
+        <h2>Last name: {user.last_name}</h2>
+        <h2>Pronouns: {user.pronouns}</h2>
+        <h2>Instrument: {user.instrument}</h2>
+        <h2>Email address: {user.email}</h2>
+        <div className="p-4">
+          {/* eslint-disable-next-line jsx-a11y/img-redundant-alt, @next/next/no-img-element */}
+          <img src={user.profile_image_url} alt="user picture" className="rounded-full" />
+        </div>
+        {user.is_teacher === false ? (
+          <>
+            <h2>Birthdate: {user.birthdate}</h2>
+            <h2>Guardian names: {user.guardian_names}</h2>
+            <Button onClick={handleClick}>View Teachers</Button>
+          </>
+        ) : ('')}
+      </div>
+    </>
   );
 }
